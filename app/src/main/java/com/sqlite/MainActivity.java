de179d76bc609531e0ffea31e04f3c727a9e4d45
rojinmiya <miyarojin557@gmail.com>
2019-04-29 11:45:15 +0545

    Initial commit

---
+++ app/src/main/java/com/sqlite/MainActivity.java
@@ -0,0 +1,39 @@
+package com.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText etWord=findViewById(R.id.etWord);
+        final EditText etMeaning=findViewById(R.id.etMeaning);
+        Button btnAddWord=findViewById(R.id.btnAddWord);
+
+        final MyHelper myHelper=new MyHelper(this);
+        final SQLiteDatabase sqLiteDatabase=myHelper.getWritableDatabase();
+
+        btnAddWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myHelper.InsertData(etWord.getText().toString(),etMeaning.getText().toString(),sqLiteDatabase))
+                {
+                    Toast.makeText(MainActivity.this,"succesafull", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
