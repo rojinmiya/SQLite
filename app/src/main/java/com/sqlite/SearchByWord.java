2b88343668a1722ad22dd37b145695feb14e100f
Softwarica College Lab <softwarica@softwarica.com>
2019-05-03 08:36:30 +0545

    final commmit

--- app/src/main/java/com/sqlite/SearchByWord.java
+++ app/src/main/java/com/sqlite/SearchByWord.java
@@ -5,11 +5,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SearchByWord extends AppCompatActivity {
@@ -40,7 +43,17 @@ public class SearchByWord extends AppCompatActivity {
 
         List<Word> wordList = new ArrayList<>();
         wordList = myHelper.GetWordByName(etWord.getText().toString(),sqLiteDatabase);
+        HashMap<String, String> hashMap = new HashMap<>();
 
+        for (int i = 0; i < wordList.size(); i++) {
+            hashMap.put(wordList.get(i).getWord(), wordList.get(i).getMeaning());
+        }
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(hashMap.keySet())
+        );
+        lvOutput.setAdapter(stringArrayAdapter);
 
 
     }
